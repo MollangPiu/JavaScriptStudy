a84bcf5ec3273ffd4e83b27aad78c8a1680c6bc1
unknown <you@example.com>
2023-12-24 02:08:03 +0900

    Feat: async 추가

--- promise/FirstPromise.js
+++ promise/FirstPromise.js
@@ -3,8 +3,8 @@ const promise = new Promise((resolve, reject) => {
 
     setTimeout(() => {
         console.log('123');
-        //resolve('success'); //성공값을 여기에 넣는다.
-        reject(new Error('no network'));    //실패값 혹은 실패 이유를 여기에 넣는다.
+        resolve('success'); //성공값을 여기에 넣는다.
+        //reject(new Error('no network'));    //실패값 혹은 실패 이유를 여기에 넣는다.
         //reject('test');
     })
 
@@ -15,4 +15,16 @@ promise.then(value => {
 })
 .catch(error => {
     console.log(error);
+});
+
+
+console.log('---------------------------------');
+const test = new Promise(function(resolve, reject) {
+   resolve('test123');
+   reject(new Error('test Error'));
+});
+
+test.then(value => {
+    console.log(test);
+    console.log(value);
 })
